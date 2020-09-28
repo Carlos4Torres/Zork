3d8e67b9d618166326a17d2b38b7c863ada54a5f
Carlos4Torres <47310346+Carlos4Torres@users.noreply.github.com>
2020-09-28 13:41:24 -0400

    Zork 2.1 Update

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Data.Common;
 using System.Linq;
 using System.Runtime.Remoting;
 
@@ -7,6 +9,13 @@ namespace Zork
 {
     class Program
     {
+        /*private static string CurrentRoom
+        {
+            get
+            {
+                return Rooms[Location.Row, Location.Column];
+            }
+        }*/
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -17,47 +26,64 @@ namespace Zork
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                //string outputString;
+                string outputString;
                 switch (command)
                 {
+                    case Commands.QUIT:
+                        outputString = ("Thank you for playing!");
+                        break;
+                    
                     case Commands.LOOK:
-                        Console.Write("This is an open field west of a white house, with a boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.");
-                        command = Commands.LOOK;
+                        outputString = ("This is an open field west of a white house, with a boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.");
                         break;
 
                     case Commands.NORTH:
-                        Console.Write("You moved North.");
-                        command = Commands.NORTH;
-                        break;
-
                     case Commands.SOUTH:
-                        Console.Write("You moved South.");
-                        command = Commands.SOUTH;
-                        break;
-
                     case Commands.EAST:
-                        Console.Write("You moved East.");
-                        command = Commands.EAST;
-                        break;
-
                     case Commands.WEST:
-                        Console.Write("You moved West.");
-                        command = Commands.WEST;
+                        outputString = $"You moved {command}.";
                         break;
 
-                    case Commands.QUIT:
-                        Console.Write("Thank you for playing!");
+                    default:
+                        outputString = "Unknown Command.";
                         break;
+                }
 
-                    case Commands.UNKNOWN:
-                        Console.Write("Unknown command.");
-                        break;
-                };
-
-                //Console.WriteLine(outputString);
+                Console.WriteLine(outputString);
 
             }       
         }
+        /*
+        private static bool Move(Commands command)
+        {
+            //Assert.IsTrue(IsDirection(command), "Invalid Direction.");
+            bool IsValidMove = true;
+            switch (command)
+            {
+                case Commands.NORTH when Location.Row < Rooms.getLength(0) - 1:
+                    Location.Row++;
+                    break;
+
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
+
+                case Commands.EAST when Location.Column < Rooms.getLength(1) - 1:
+                    Location.Column++;
+                    break;
+
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
+                    break;
+
+                default:
+                    IsValidMove = false;
+                    break;
+
+            }
+
+            return IsValidMove;
+        }*/
 
         private static Commands ToCommand(string commandString)
         {
