f5dd99ab0901cfe01e33048a6dc1d2f1d24bc0dc
Carlos4Torres <47310346+Carlos4Torres@users.noreply.github.com>
2020-10-03 15:33:55 -0400

    Zork 3.1 Update

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,7 +9,22 @@ namespace Zork
 {
     internal class Program
     {
-        private static string CurrentRoom
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the other windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all direction around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house, there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the other windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+
+        }
+        public static Room CurrentRoom
         {
             get
             {
@@ -18,6 +33,8 @@ namespace Zork
         }
         static void Main(string[] args)
         {
+            Program.InitializeRoomDescriptions(); 
+
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
@@ -34,7 +51,7 @@ namespace Zork
                         break;
                     
                     case Commands.LOOK:
-                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door. ");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
@@ -43,7 +60,7 @@ namespace Zork
                     case Commands.WEST:
                         if (Move(command) == false)
                         {
-                            Console.WriteLine("The way is shut!");
+                            Console.WriteLine("The way is blocked!");
                         }
                         break;
 
@@ -90,19 +107,20 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static readonly string[,] Rooms =
-        {
-            {"Rocky Trail", "South of the House", "Canyon View"},
-            {"Forest", "West of the House", "Behind the House" },
-            {"Dense Woods", "North of House", "Clearing" }
+        private static readonly Room[,] Rooms = {
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+
         };
 
-    private static readonly List<Commands> Directions = new List<Commands>
+        private static readonly List<Commands> Directions = new List<Commands>
         {
             Commands.NORTH,
             Commands.SOUTH,
             Commands.EAST,
-            Commands.WEST
+            Commands.WEST,
+            Commands.LOOK
         };
 
         private static (int Row, int Column) Location = (1, 1);
